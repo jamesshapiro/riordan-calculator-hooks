6bd05d008beaf884ba60ee0b3d7e399d8fa72387
James Shapiro <jamesshapiro@gmail.com>
2024-04-30 18:40:38 -0400

    add date to table

--- src/components/UserHistory/UserHistory.js
+++ src/components/UserHistory/UserHistory.js
@@ -3,33 +3,52 @@ import React from 'react';
 import styled from 'styled-components';
 import { UserContext } from '../UserProvider';
 
+function formatDate(dateString) {
+  const date = new Date(dateString);
+
+  const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+  const timeOptions = { hour: 'numeric', minute: 'numeric', hour12: true };
+
+  const formattedDate = new Intl.DateTimeFormat('en-US', dateOptions).format(
+    date
+  );
+  const formattedTime = new Intl.DateTimeFormat('en-US', timeOptions).format(
+    date
+  );
+  return dateString;
+  return `${formattedDate}, ${formattedTime}`;
+}
+
 function UserHistory() {
-  const { userQueries, isAuthorized } =
-    React.useContext(UserContext);
+  const { userQueries, isAuthorized } = React.useContext(UserContext);
   if (userQueries.length < 1) {
-    return <></>
+    return <></>;
   }
   return (
     <Wrapper>
       <thead>
         <tr>
-        <TDWrapper>Link</TDWrapper>
+          <TDWrapper>Permalink</TDWrapper>
+          <TDWrapper>Date</TDWrapper>
           <TDWrapper>G</TDWrapper>
           <TDWrapper>F</TDWrapper>
-          
         </tr>
       </thead>
       <tbody>
-        {userQueries.map((query, index) => (
-          <tr key={index}>
-            <TDWrapper><a href={`/?${query.MATRIX_SHAREID.S}`}>{query.MATRIX_SHAREID.S}</a></TDWrapper>
-            <TDWrapper>{query.G_SEQUENCE.S}</TDWrapper>
-            <TDWrapper>{query.F_SEQUENCE.S}</TDWrapper>
-            
-          </tr>
-        ))}
+        {userQueries.map((query, index) => {
+          console.log(query);
+          return (
+            <tr key={index}>
+              <TDWrapper>
+                <Permalink href={`/?${query.MATRIX_SHAREID.S}`}>Link</Permalink>
+              </TDWrapper>
+              <TDWrapper>{formatDate(query.CREATED_AT.S)}</TDWrapper>
+              <TDWrapper>{query.G_SEQUENCE.S}</TDWrapper>
+              <TDWrapper>{query.F_SEQUENCE.S}</TDWrapper>
+            </tr>
+          );
+        })}
       </tbody>
-
     </Wrapper>
   );
 }
@@ -37,8 +56,8 @@ function UserHistory() {
 export default UserHistory;
 
 const Wrapper = styled.table`
-background-color: #f1f1f1;
-border: 1px solid black;
+  background-color: #f1f1f1;
+  border: 1px solid black;
   border-collapse: collapse;
   width: 100%;
   margin-top: 200px;
@@ -47,4 +66,13 @@ border: 1px solid black;
 const TDWrapper = styled.td`
   border: 1px solid black;
   padding: 8px;
-`
+`;
+
+const Permalink = styled.a`
+  text-decoration: none;
+
+  &:visited {
+    color: blue;
+    text-decoration: none;
+  }
+`;
